b171a3ddf44f8ed1b6f1e1cbbb86bb24630223f3
Joey Cos <joeycosentino24@gmail.com>
2022-08-15 13:59:55 -0400

    added styling and content to intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import '../scss/intro.scss';
 
 function Intro() {
     return (
         <div className='intro' id='intro'>
-            intro
+            <div className='left'>
+                <div className='imageContainer'>
+                    <img src='assets/me.jpg' alt='' />
+                </div>
+            </div>
+            <div className='right'>
+                <div className='wrapper'>
+                    <h2>Hi there, I'm</h2>
+                    <h1>Joe Cosentino</h1>
+                    <h3>Full Stack Web Developer<span></span></h3>
+                </div>
+                <a href='#portfolio'>
+                    <KeyboardArrowDownIcon sx={{ fontSize: 70 }} className='down-arrow'/>
+                </a>
+            </div>
         </div>
     )
 }
