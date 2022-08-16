78db5c07d5fd116fc1b73c10faec993da8582420
Joey Cos <joeycosentino24@gmail.com>
2022-08-15 22:33:07 -0400

    began building portfolio component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,7 +4,19 @@ import '../scss/portfolio.scss';
 function Portfolio() {
     return (
         <div className='portfolio' id='portfolio'>
-
+            <h1>Portfolio</h1>
+            <ul>
+                <li>Featured</li>
+                <li>Web app</li>
+                <li>Mobile App</li>
+                <li>Design</li>
+                <li>Branding</li>
+            </ul>
+            <div className='container'>
+                <div className='item'></div>
+                <img src='' alt='' />
+                <h3>Banking App</h3>
+            </div>
         </div>
     )
 }
