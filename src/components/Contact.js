51f5f126e79e92592f9d9aac70298340a85a48d5
Joey Cos <joeycosentino24@gmail.com>
2022-08-19 12:21:46 -0400

    contact form is now working and sends emails to me directly from portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,28 @@
 import React, { useState } from 'react';
+import emailjs from 'emailjs-com'
 import '../scss/contact.scss';
 
 function Contact() {
 
     const [message, setMessage] = useState(false);
 
-    const handleSubmit = e => {
+    // const handleSubmit = e => {
+    //     e.preventDefault();
+    //     setMessage(true);
+    // }
+
+    function sendEmail(e) {
         e.preventDefault();
         setMessage(true);
-    }
+    
+        emailjs.sendForm('service_n4lu94u', 'template_j7jiofy', e.target, 'bAQOi6YWqkv0Ch6Aw')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset();
+      };
 
     return (
         <div className='contact' id='contact'>
@@ -17,9 +31,9 @@ function Contact() {
             </div>
             <div className='right'>
                 <h2>Contact.</h2>
-                <form onSubmit={handleSubmit}>
-                    <input type='text' placeholder='email' />
-                    <textarea placeholder='message'></textarea>
+                <form onSubmit={sendEmail}>
+                    <input type='text' name='email' placeholder='email' />
+                    <textarea name='message' placeholder='message'></textarea>
                     <button type='submit'>Send</button>
                     {message && <span>Thanks! I will reply as soon as I can!</span>}
                 </form>                    
